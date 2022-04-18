4b1dc58ddfd32f9ec21ce4a71f9b6ce58c070a7d
Artur Krawczyk <artur.damian.krawczyk@gmail.com>
2022-04-18 19:21:55 +0200

    Improved JS added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://kodilla-tasks-ak.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'https://api.trello.com/1';
+  const trelloApiRoot = 'https://api.trello.com/1/members/arturdamiankrawczyk1';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
